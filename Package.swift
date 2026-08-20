769daa8ea34173793aa5f53059e950e5efbcea36
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:07:12 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-io-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -21,18 +21,16 @@ let package = Package(
             targets: ["IO Primitives Test Support"]
         ),
     ],
-    dependencies: [
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "IO Primitives",
-            dependencies: [
-            ]
+            dependencies: []
         ),
         .target(
             name: "IO Primitives Test Support",
             dependencies: [
-                "IO Primitives",
+                "IO Primitives"
             ],
             path: "Tests/Support"
         ),
